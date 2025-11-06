082b40270ff456338133c49180af484db0260053
Valentin Alejandro Gimenez <valentingimenez1909@gmail.com>
2025-11-05 21:37:06 -0300

    bd host

--- Models/InquilinoModel.cs
+++ Models/InquilinoModel.cs
@@ -9,11 +9,11 @@ public class Inquilino
     [RegularExpression(@"^[\p{L}]+$", ErrorMessage = "El nombre no puede contener números ni caracteres especiales")]
     public string nombre { get; set; }= string.Empty;
     
- [Required(ErrorMessage = "Ingrese un Apellido")]
+   [Required(ErrorMessage = "Ingrese un Apellido")]
     [RegularExpression(@"^[\p{L}]+$", ErrorMessage = "El apellido no puede contener números ni caracteres especiales")]
     public string apellido { get; set; }= string.Empty;
   
- [Required(ErrorMessage = "Ingrese un DNI")]
+   [Required(ErrorMessage = "Ingrese un DNI")]
     [RegularExpression(@"^\d{8}$", ErrorMessage = "El DNI no es válido")]
     public string dni { get; set; }= string.Empty;
      [Required(ErrorMessage = "Ingrese un Email")]
@@ -24,6 +24,7 @@ public class Inquilino
    [RegularExpression(@"^(\d{8,12})?$", ErrorMessage = "El teléfono no es válido")]
     public string telefono { get; set; }= string.Empty;
     public int estado { get; set; }
+    public string? imagen { get; set; }
 public string? NombreCompleto => $"{nombre} {apellido}";
 
 
